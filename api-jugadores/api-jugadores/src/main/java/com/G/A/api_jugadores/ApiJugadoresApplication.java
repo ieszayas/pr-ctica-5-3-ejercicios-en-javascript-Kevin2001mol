98afa6b5301529c388115a0ebc158d8673bffcee
kevin2001mol <kevin2001molina@gmail.com>
2025-03-01 18:50:51 +0100

    Inserción de la api

---
+++ api-jugadores/api-jugadores/src/main/java/com/G/A/api_jugadores/ApiJugadoresApplication.java
@@ -0,0 +1,13 @@
+package com.G.A.api_jugadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJugadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJugadoresApplication.class, args);
+	}
+
+}
